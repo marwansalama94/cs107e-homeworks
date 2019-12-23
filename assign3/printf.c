abe2b4b5adc4b514a63f60583e967e32cc2cfc66
Marwan Salama <marwansalama94@gmail.com>
2019-12-23 06:38:25 +0200

    finalized 'strings.c' added partial solution to 'printf.c'

--- assign3/printf.c
+++ assign3/printf.c
@@ -1,11 +1,84 @@
 #include "printf.h"
+#include "string.h"
+
+static unsigned int offset_calc(int digit_count, int bufsize, int min_width){
+    if(digit_count < bufsize && digit_count >= min_width){
+        return digit_count;
+    }else if (min_width > digit_count && min_width < bufsize){
+        return min_width;
+    }else{
+        return bufsize;
+    }
+}
+
+
+int unsigned_to_base(char *buf, int bufsize, unsigned int val, int base, int min_width){
+    unsigned int ncount = 0;
+    int offset;
+    int digit_count = digits_count(val);
+    if(!bufsize)return 0;
+    offset = offset_calc(digit_count, bufsize, min_width);
+    for (int i = 1 ; i <= offset; i++){
+        *(buf+offset-i) = ((val%base)<10)? '0'+ val % base : 'A'+ (val%base)%10 ;
+        val /= base;
+        ncount++;
+    }
+    *(buf+offset) = '\0';
+    return ncount;
+}
+
+int signed_to_base(char *buf, int bufsize, int val, int base, int min_width){
+    if(val < 0){
+        *buf = '-';
+        return unsigned_to_base(buf+1,bufsize-1, (val*-1),base,min_width-1)+1;
+    }
+    return unsigned_to_base(buf,bufsize,val,base,min_width);
+}
 
 int vsnprintf(char *buf, int bufsize, const char *format, va_list args){
     //TODO:to implement
 }
 
 int snprintf(char *buf, int bufsize, const char *format, ...) __attribute__ ((format (printf, 3, 4))){
-    //TODO:to implement
+    //TODO:differentiate between the %p %d and %x and also to clean up the code a lil bit
+    int count = 0;
+    va_list ap;
+    va_start(ap,format);
+
+    while(((bufsize-1)-count) && *format){
+        if (*format != '%'){
+            *(buf+count) = *(format);//it is just a character
+            format++;
+            count++;
+        }
+        else{//case: placeholder
+            format++;
+            const char * ptr;
+            int offset;
+            switch(*format){
+                case 'c':
+                    *(buf+count) = (char)va_arg(ap,int);
+                    format++;
+                    count++;
+                    break;
+                case 's':
+                    count+= strlcat(buf+count,va_arg(ap,char*),bufsize-count-1);
+                    format++;
+                    break;
+                default:
+                    //offset = strlcat((buf+count), "0x",3) + signed_to_base(buf+count+2, bufsize-count-1, va_arg(ap,int), 16, strtonum(format,&ptr)-2);
+                    offset = signed_to_base(buf+count, bufsize-count-1, va_arg(ap,int), 10, strtonum(format,&ptr));
+                    count+= offset;
+                    format+= (ptr-format)+1;
+                break;
+            }
+        }
+
+    }
+
+    va_end(ap);
+    *(buf+count) = '\0';
+    return count;
 }
 
 int printf(const char *format, ...) __attribute__ ((format (printf, 1, 2))){
--- assign3/strings.c
+++ assign3/strings.c
@@ -1,25 +1,89 @@
 #include "strings.h"
 
 void *memset(void *s, int c, size_t n){
-    //TODO:to implement
+    for (size_t i = 0; i < n ; i++){
+      *( ((unsigned char*)s) + i) = (unsigned char)c;
+    }
+    return s;
 }
 
 void *memcpy(void *dst, const void *src, size_t n){
-    //TODO:to implement
+    for(size_t i = 0; i < n; i++){
+        *( ((unsigned char*)dst) + i ) = *( ((unsigned char*)src) + i );
+    }
+    return dst;
 }
 
 int strlen(const char *s){
-    //TODO:to implement
+    int n = 0;
+    while(*s++){
+        n++;
+    }
+    return n;
 }
 
 int strcmp(const char *s1, const char *s2){
-    //TODO:to implement
+    int s1_length = strlen(s1);
+    int s2_length = strlen(s2);
+    if (s1_length != s2_length) return s1_length - s2_length;
+
+    while( (*s1 == *s2) && *s1){
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
+}
+
+static unsigned int strtonum_hex(const char *str,const char **endptr){
+    unsigned int num = 0;
+    str+= 2;
+    while(*str){
+        num<<= 4;//a place to get the new 4 bits
+        if(*str >= '0' && *str <='9'){
+            num += ((*str)-'0');
+        }else if (*str >= 'A' && *str <='F'){
+            num += 10+((*str)-'A');
+        }else if (*str >= 'a' && *str <='f'){
+            num += 10+((*str)-'a');
+        }else{
+            *endptr = str;
+            break;
+        }
+        str++;
+    }
+    return num;
+}
+
+static unsigned int strtonum_decimal(const char *str, const char** endptr){
+    unsigned int num = 0;
+    while(*str){
+        if(*str >= '0' && *str <='9'){
+            num*= 10;
+            num += *str-'0';
+        }else{
+            *endptr = str;
+            break;
+        }
+        str++;
+    }
+    return num;
 }
 
 unsigned int strtonum(const char *str, const char **endptr){
-    //TODO:to implement
+    if( str[0] == '0' && (str[1] == 'x' || str[1] == 'X') ){
+        return strtonum_hex(str,endptr);
+    }else{
+        return strtonum_decimal(str,endptr);
+    }
 }
 
 int strlcat(char *dst, const char *src, int maxsize){
-    //TODO:to implement
+    int end_of_dst = strlen(dst);
+    int offset = 0;
+    while(*(src+offset) && (maxsize-(end_of_dst+offset+1)) > 0){
+        *(dst+ end_of_dst + offset) = *(src+offset);
+        offset++;
+    }
+    *(dst+end_of_dst+offset)='\0';
+    return (end_of_dst+offset);
 }
